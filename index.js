5ea712217a8eb02f5eabf3a7d82d3e55c9feea12
Gabriel Marinho dos Santos <152182959+gMarinhum085@users.noreply.github.com>
2024-08-15 11:01:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,25 +4,22 @@ const Author = require('./Author')
 const Post = require('./Post')
 
 const author1 = new Author(
-    'Jonh Wick',
+    'Gabriel Marinho',
 )
 
 const post1 = new Post(
-    'Título do post tal',
-    'Conteúdo do post tal',
+    'Título do post de Gabriel ', 'Conteúdo do post e Gabriel',
     author1
 )
 
 
 
 const comment1 = new Comment(
-    'Winston',
-    'Que legal!'
+    'Winston', 'Que legal!'
 )
 
 const comment2 = new Comment(
-    'Frankie',
-    'Você de novo por aqui.'
+    'Frankie', 'Você de novo por aqui.'
 )
 
 author1.addPost(post1)
